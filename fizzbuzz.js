31bd6473188fe0baae3b520de1e223ffed45c4ef
Xiusi Li <xiusi.li@softwire.com>
2019-12-09 14:11:34 +0000

    Fizzbuzz everything (until 17)

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -5,20 +5,57 @@ function fizzbuzz() {
     
     console.log('Welcome to FizzBuzz!');
 
-    for (i=1; i<100; i++){
+    for (i=1; i<200; i++){
+        
+        let x=i
+
+
         if (i%3===0 && i%5 ===0){
-            console.log('FizzBuzz')
+            x='FizzBuzz'
         }
         else if ( i%3 === 0){
-            console.log('Fizz')
+            x= 'Fizz'
         }
+
+
         else if ( i%5 === 0){
-            console.log('Buzz')
+            x='Buzz'
         }
-        else{
-            console.log(i)
+        
+
+        if (i%7 ===0 && x!=i){
+            x=x+'Bang'
         }
+
+        if(i%7===0 && x==i){
+            x='Bang'
+        }
+
+        if(i%11 ===0){
+            x='Bong'
+        }
+
+        if (i%13 ===0){
+            if(x==i){
+                x='Fezz'
+            }
+            else if (x.search('B')!=-1){
+                x=[x.slice(0,x.search('B')),'Fezz',x.slice(x.search('B'))].join('')
+            }
+
+            else {
+                x=x+'Fezz'
+            }
+
+        }
+
+
+
+
+        
+    console.log(x)
     }
+
 
     
 }
