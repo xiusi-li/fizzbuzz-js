313c057ff3f2a8a1ea61bf9cf983a9370fedf658
Xiusi Li <xiusi.li@softwire.com>
2019-12-09 13:38:27 +0000

    part 1

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -5,7 +5,20 @@ function fizzbuzz() {
     
     console.log('Welcome to FizzBuzz!');
 
-    // Put your code here...
+    for (i=1; i<100; i++){
+        if (i%3===0 && i%5 ===0){
+            console.log('FizzBuzz')
+        }
+        else if ( i%3 === 0){
+            console.log('Fizz')
+        }
+        else if ( i%5 === 0){
+            console.log('Buzz')
+        }
+        else{
+            console.log(i)
+        }
+    }
 
     
 }
