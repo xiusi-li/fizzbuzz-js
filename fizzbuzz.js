7b85bbcb7001ac2893d1ca2705f0960b79bcbb2d
Xiusi Li <xiusi.li@softwire.com>
2019-12-09 15:45:22 +0000

    create rule for Fezz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -5,55 +5,56 @@ function fizzbuzz() {
     
     console.log('Welcome to FizzBuzz!');
 
-    for (i=1; i<200; i++){
+    for (i=1; i<300; i++){
         
-        let x=i
+        let output=[];
 
 
-        if (i%3===0 && i%5 ===0){
-            x='FizzBuzz'
-        }
-        else if ( i%3 === 0){
-            x= 'Fizz'
+        if ( i%3 === 0){
+            output.push('Fizz');
         }
 
-
-        else if ( i%5 === 0){
-            x='Buzz'
+        if ( i%5 === 0){
+            output.push('Buzz');
         }
         
 
-        if (i%7 ===0 && x!=i){
-            x=x+'Bang'
-        }
-
-        if(i%7===0 && x==i){
-            x='Bang'
+        if (i%7 === 0){
+            output.push('Bang');
         }
 
         if(i%11 ===0){
-            x='Bong'
+            output=['Bong'];
         }
 
-        if (i%13 ===0){
-            if(x==i){
-                x='Fezz'
-            }
-            else if (x.search('B')!=-1){
-                x=[x.slice(0,x.search('B')),'Fezz',x.slice(x.search('B'))].join('')
+        if (i%13 === 0){
+            let lengthBeforeFezz=output.length;
+            let fezzAdded = false;
+            
+            for (j=0; j<lengthBeforeFezz; ++j) {
+                let wordInOutput=output[j];
+                if (wordInOutput.substring(0,1)==='B'){
+                    output=output.slice(0,j).concat(['Fezz']).concat(output.slice(j));
+                    fezzAdded = true;
+                    break;
+                }
             }
 
-            else {
-                x=x+'Fezz'
+            if (fezzAdded === false){
+                output.push('Fezz');
             }
-
         }
 
+        if (i%17 ===0){
+            output.reverse();
+        }
 
-
-
-        
-    console.log(x)
+    if (output.length===0){
+        console.log(i);
+    }
+    else{
+        console.log(output.join(''));
+    }
     }
 
 
